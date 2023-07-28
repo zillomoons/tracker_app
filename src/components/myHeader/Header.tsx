7ac8838e1ea4feb169959608588546030eda0239
Zillomoons <dlzht73@ya.ru>
2023-07-28 23:02:12 +0800

    refactoring styles

--- src/components/myHeader/Header.tsx
+++ src/components/myHeader/Header.tsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
-import s from './Header.module.css';
 
 export function Header() {
   return (
-    <header className={s.header}>
-      <Link to='/'>Habit Tracker</Link>
-      <Link to='/settings'>Settings</Link>
+    <header>
+      <div className='wrapper'>
+        <Link to='/'>Habit Tracker</Link>
+        <Link to='/settings'>Settings</Link>
+      </div>
     </header>
   );
 }
