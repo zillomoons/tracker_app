9d2e5fd915fe556a6a06704135da2af3d1385d8e
Zillomoons <dlzht73@ya.ru>
2023-08-07 09:52:21 +0800

    added signout func

--- src/components/myHeader/Header.tsx
+++ src/components/myHeader/Header.tsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
 import { RxExit } from 'react-icons/rx';
 import { FaRegUserCircle } from 'react-icons/fa';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { selectAuth, signOut } from '../../features/auth/authSlice';
 
 export function Header() {
-  const signOut = () => console.log('sign out');
+  const dispatch = useAppDispatch();
+  const onSignOut = () => {
+    void dispatch(signOut());
+  };
+  const { session } = useAppSelector(selectAuth);
   const userAvatar = '';
   return (
     <header>
@@ -11,16 +17,19 @@ export function Header() {
         <h3 className='hand-script'>
           <Link to='/'>Habit Tracker</Link>
         </h3>
-        <div className='flex'>
-          <Link to='/settings'>
-            {userAvatar ? (
-              <img src={userAvatar} alt='grogu' className='user-image' />
-            ) : (
-              <FaRegUserCircle />
-            )}
-          </Link>
-          <RxExit onClick={signOut} />
-        </div>
+
+        {session && (
+          <div className='flex'>
+            <Link to='/profile'>
+              {userAvatar ? (
+                <img src={userAvatar} alt='grogu' className='user-image' />
+              ) : (
+                <FaRegUserCircle />
+              )}
+            </Link>
+            <RxExit onClick={onSignOut} />
+          </div>
+        )}
       </div>
     </header>
   );
